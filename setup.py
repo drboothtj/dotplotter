4c45fecad11de243f5533ea59460baa51bff59eb
drboothtj <booth.thom@gmail.com>
2024-04-09 15:08:51 +0200

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="dotplotter",
-    version="0.0.1",
+    version="1.0.0",
     author="Thomas J. Booth",
     author_email="thoboo@biosustain.dtu.dk",
     packages=find_packages(),
